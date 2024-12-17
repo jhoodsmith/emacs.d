0d1ccf2cd9fdd55593a069e6cb4119e7579be21b
James Hood-Smith <james.hood-smith@smartpension.co.uk>
2024-12-17 22:45:04 +0000

    Removed K8s configuration

--- init.el
+++ init.el
@@ -609,12 +609,4 @@
 ;; Configure warnings
 (setq-default warning-minimum-level :error)
 
-;; Kubernetes
-
-(use-package kubernetes
-  :commands (kubernetes-overview)
-  :config
-  (setq kubernetes-poll-frequency 3600
-        kubernetes-redraw-frequency 3600))
-
 ;;; init.el ends here
